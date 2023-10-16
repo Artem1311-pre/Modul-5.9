86a808b32c5705790c52c3fa4892697d105e55fe
Artem1311-pre <akochkin@toyotatt.ru>
2023-10-16 15:56:05 +0300

    В приложение не загружаются посты

--- Newspaper/Newspaper/settings.py
+++ Newspaper/Newspaper/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.flatpages',
 ]
 
-SITE_ID  = 1
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -127,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
--- Newspaper/news/models.py
+++ Newspaper/news/models.py
@@ -1,47 +1,83 @@
 from django.db import models
+from django.db.models import Sum
 from django.contrib.auth.models import User
 
 class Author(models.Model):
-    name = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     rating = models.IntegerField(default=0)
 
     def update_rating(self):
-        post_comments_rating = self.posts.aggregate(models.Sum('comments__rating')).get('comments__rating__sum') or 0
-        self.rating = (self.posts.aggregate(models.Sum('rating')).get('rating__sum') or 0) * 3
-        self.rating += post_comments_rating
+
+        articles_rating = \
+            Post.objects.filter(author_id=self.id).aggregate(Sum('rating_news')).get('rating_news__sum') * 3
+        # print(articles_rating)
+        comments_rating = \
+            Comment.objects.filter(author_id=self.id).aggregate(Sum('comment_rating')).get('comment_rating__sum')
+        # print(comments_rating)
+        comments_articles_rating = \
+            Comment.objects.filter(post__author=self).aggregate(Sum('comment_rating')).get('comment_rating__sum')
+        # print(comments_articles_rating)
+
+        self.rating = articles_rating + comments_rating + comments_articles_rating
         self.save()
+
+    def __str__(self):
+        return f'{self.user.username}'
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+
+    sport = 'SP'
+    policy = 'PO'
+    education = 'ED'
+    culture = 'CU'
+    technology = "TECH"
+
+    SUBJECTS = [
+            ("policy", 'Политика'),
+            ("culture", 'Культура'),
+            ("education", 'Образование'),
+            ("sport", 'Спорт'),
+            ("technology", 'Технологии')
+    ]
+
+    subject = models.CharField(max_length=10, choices=SUBJECTS, default=sport, unique=True)
 
     def __str__(self):
-        return self.name
+        return self.subject.title()
 
+Article = "AT"
+News = "NW"
+
+POST_TYPES = [
+    ('Article', 'Статья'),
+    ('News', 'Новость'),
+]
 
 class Post(models.Model):
-    POST_TYPES = [
-        ('Article', 'Article'),
-        ('News', 'News'),
-    ]
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='posts')
-    post_type = models.CharField(max_length=10, choices=POST_TYPES)
-    created_date = models.DateTimeField(auto_now_add=True)
-    categories = models.ManyToManyField(Category, through='PostCategory')
+
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    article_news = models.CharField(max_length=10, choices=POST_TYPES, default=News)
+    date_in = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=255)
     text = models.TextField()
-    rating = models.IntegerField(default=0)
+    rating_news = models.IntegerField(default=0)
+    category = models.ManyToManyField(Category, through='PostCategory')
 
     def like(self):
-        self.rating += 1
+        self.rating_news += 1
         self.save()
 
     def dislike(self):
-        self.rating -= 1
+        self.rating_news -= 1
         self.save()
 
     def preview(self):
-        return self.text[:124] + '...' if len(self.text) > 124 else self.text
+        return self.text[0:124] + '...'
+
+    def __str__(self):
+        return f'{self.title.title()}:{self.text.title()}:{self.date_in}'
 
 
 class PostCategory(models.Model):
@@ -50,19 +86,18 @@ class PostCategory(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField(default=0)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment_text = models.TextField(blank=False)
+    comment_time_in = models.DateTimeField(auto_now_add=True)
+    comment_rating = models.IntegerField(default=0)
 
     def like(self):
-        self.rating += 1
+        self.comment_rating += 1
         self.save()
 
     def dislike(self):
-        self.rating -= 1
+        self.comment_rating -= 1
         self.save()
-
 
 # Create your models here.
