5b6c300f16f1c97324f10b183a9779ff03db77b9
Artem1311-pre <akochkin@toyotatt.ru>
2023-10-27 17:49:00 +0300

    не работает поиск

--- Newspaper/news/views.py
+++ Newspaper/news/views.py
@@ -1,12 +1,28 @@
 from datetime import datetime
 from django.views.generic import ListView, DetailView
 from .models import Post
+from .filters import PostFilter
+from django.core.paginator import Paginator
+
 
 class PostList(ListView):
     model = Post
     ordering = 'title'
     template_name = 'news.html'
     context_object_name = 'news'
+    paginate_by = 1
+
+    def get_queryset(self):
+        # Получаем обычный запрос
+        queryset = super().get_queryset()
+        # Используем наш класс фильтрации.
+        # self.request.GET содержит объект QueryDict, который мы рассматривали
+        # в этом юните ранее.
+        # Сохраняем нашу фильтрацию в объекте класса,
+        # чтобы потом добавить в контекст и использовать в шаблоне.
+        self.filterset = PostFilter(self.request.GET, queryset)
+        # Возвращаем из функции отфильтрованный список товаров
+        return self.filterset.qs
 
 
     def get_context_data(self, **kwargs):
